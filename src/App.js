eb41d2b6845621e57a6fd0e2f87882c403d5111b
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-15 19:22:45 -0400

    initialized state and passed down props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-
-import './App.scss';
+import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Summary from './pages/Summary/Summary';
-import Notes from './pages/Notes/Notes';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Summary from "./pages/Summary/Summary";
+import Notes from "./pages/Notes/Notes";
 
 function App() {
+  const [tsData, settsData] = useState("");
+  const [patientId, setpatientId] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080")
+      .then((res) => {
+        settsData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8080")
+  //     .then((res) => {
+  //       setpatientId(res.data.id);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Summary />} />
-        <Route path="/notesId" element={<Notes />} />
+        <Route path="/notesId" element={<Notes tsData = {tsData} patientId={patientId} />} />
       </Routes>
     </BrowserRouter>
   );
