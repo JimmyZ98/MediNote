a8edf43b05fd7133fd904bc09f2e03123168a511
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-16 02:27:25 -0400

    highlight words functionality limited to 10 words successful

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 import Summary from './pages/Summary/Summary';
 import Notes from './pages/Notes/Notes';
 import Header from './components/Header/Header'
@@ -10,12 +7,6 @@ import Header from './components/Header/Header'
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-=======
-=======
->>>>>>> feature/fetch-api
-import Summary from "./pages/Summary/Summary";
-import Notes from "./pages/Notes/Notes";
->>>>>>> 046fa30 (retrieve transcribed data from backend working)
 
 function App() {
 
--- src/pages/Notes/Notes.js
+++ src/pages/Notes/Notes.js
@@ -1,10 +1,9 @@
 import "./Notes.scss";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Recorder from "../../components/Recorder/Recorder";
 
 function Notes() {
-  const [tsData, settsData] = useState("asd");
+  const [tsData, settsData] = useState("");
   useEffect(() => {
     axios
       .get("http://localhost:8080")
@@ -15,49 +14,48 @@ function Notes() {
         console.log(err);
       });
   }, []);
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-
-function Notes({ tsData, patientId }) {
-    tsData
-      .filter((data) => data.id == patientId)
-      .map((data) => {
-        return (
-          <>
-            <div className="notes"> 
-              <div className="notes__header">
-                <img
-                  className="notes__logo"
-                  src={require("../../assets/Images/Logo.png")}
-                />
-                <button className="notes__record-button">
-                  Record new Patient Note
-                </button>
-
-              </div>
-              <div className="notes__container">
-                <div className="notes__patient">
-                  <div className="notes__patient-info">
-                    <div className="notes__patient-infotop">
-                      <div className="notes__patient-name">
-                        Patient Name: Lydia Shan 
-=======
-  {
-    tsData
-      .filter((data) => data.id == patientId)
-      .map((data) => {
-=======
-
-  {
-    tsData
-      .filter((data) => data.id == patientId)
-      .map((data) => {
->>>>>>> feature/fetch-api
+  // text highlight function
+  function Highlight({ children: text = "", tags = [] }) {
+    if (!tags?.length) return text;
+    const matches = [...text.matchAll(new RegExp(tags.join("|"), "ig"))]; // ig = insensitive(case) + global(duplicate) flag
+    const startText = text.slice(0, matches[0]?.index);
+    return (
+      <span>
+        {startText}
+        {matches.map((match, i) => {
+          const startIndex = match.index;
+          const currentText = match[0];
+          const endIndex = startIndex + currentText.length;
+          const nextIndex = matches[i + 1]?.index;
+          const untilNextText = text.slice(endIndex, nextIndex);
+          return (
+            <span key={i}>
+              <mark>{currentText}</mark>
+              {untilNextText}
+            </span>
+          );
+        })}
+      </span>
+    );
+  }
+  
+  // {
+  //   tsData
+  //     .filter((data) => data.id == patientId)
+  //     .map((data) => {
+    let highlightedWords = []
         if (tsData) {
+          console.log(tsData)
+          tsData.data.auto_highlights_result.results.map((highlight) => {
+            if (highlightedWords.length < 10) {
+              highlightedWords.push(highlight.text)
+            console.log(highlight.text)
+            }
+          })
+          console.log(highlightedWords)
           return (
             <>
-              <Recorder />
               <div className="notes">
                 <div className="notes__header">
                   <img
@@ -67,80 +65,6 @@ function Notes({ tsData, patientId }) {
                   <button className="notes__record-button">
                     Record new Patient Note
                   </button>
-<<<<<<< HEAD
-                </div>
-                <div className="notes__container">
-                  <div className="notes__patient">
-                    <div className="notes__patient-info">
-                      <div className="notes__patient-infotop">
-                        <div className="notes__patient-name">
-                          Patient Name: Lydia Shan
-                        </div>
-                        <div className="notes__patient-date">Oct. 15, 2022</div>
-                      </div>
-                      <div className="notes__patient-infobottom">
-                        <div className="notes__patient-type">
-                          Type of Appointment: General Visit
-                        </div>
-                        <div className="notes__patient-purpose">
-                          Purpose of Visit: Abdominal Pain
-                        </div>
->>>>>>> 046fa30 (retrieve transcribed data from backend working)
-                      </div>
-                    </div>
-                    <div className="notes__bottom">
-                      <div className="notes__patient-notes">
-                        <h2 className="notes__patient-heading">
-                          Doctor's Note
-                        </h2>
-                        <p>{tsData.data.text}</p>
-                      </div>
-                      <div className="notes__categories">
-                        <div className="notes__keywords">Keywords</div>
-                      </div>
-                    </div>
-                  </div>
-<<<<<<< HEAD
-
-            <div className="notes__bottom">
-              <div className="notes__patient-notes">
-                <h2 className="notes__patient-heading">Doctor's Note</h2>
-                <p></p>
-              </div>
-              <div className="notes__categories">
-                <div className="notes__keywords">Keywords</div>
-                <div className="notes__keywords">Medical Condition</div>
-              </div>
-            </div>
-          </div>
-          <div className="notes__buttons">
-            <button className="notes__buttons-delete">Delete</button>
-            <div className="notes__buttons-container">
-              <button className="notes__buttons-record">Record Again</button>
-              <button className="notes__buttons-edit">Edit Note</button>
-              <button className="notes__buttons-share">Share Note</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )});
-
-=======
-                  <div className="notes__buttons">
-                    <button className="notes__buttons-delete">Delete</button>
-                    <div className="notes__buttons-container">
-                      <button className="notes__buttons-record">
-                        Record Again
-                      </button>
-                      <button className="notes__buttons-edit">Edit Note</button>
-                      <button className="notes__buttons-share">
-                        Share Note
-                      </button>
-                    </div>
-                  </div>
-                </div>
-=======
                 </div>
                 <div className="notes__container">
                   <div className="notes__patient">
@@ -165,7 +89,8 @@ function Notes({ tsData, patientId }) {
                         <h2 className="notes__patient-heading">
                           Doctor's Note
                         </h2>
-                        <p>{tsData.data.text}</p>
+                        <Highlight tags={highlightedWords}>{tsData.data.text}</Highlight>
+                        <p></p>
                       </div>
                       <div className="notes__categories">
                         <div className="notes__keywords">Keywords</div>
@@ -185,14 +110,11 @@ function Notes({ tsData, patientId }) {
                     </div>
                   </div>
                 </div>
->>>>>>> feature/fetch-api
               </div>
             </>
           );
         }
-      });
-  }
->>>>>>> 046fa30 (retrieve transcribed data from backend working)
+  //     });
+  // }
 }
-
 export default Notes;
