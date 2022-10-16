046fa3075cfe472864b56cc90b0485b392f7f0fe
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-16 00:32:54 -0400

    retrieve transcribed data from backend working

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Summary from "./pages/Summary/Summary";
 import Notes from "./pages/Notes/Notes";
 
 function App() {
-  const [tsData, settsData] = useState("");
-  const [patientId, setpatientId] = useState(null)
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080")
-      .then((res) => {
-        settsData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:8080")
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-  // console.log(tsData)
-  // console.log(patientId)
-if (patientId != null) {
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Summary />} />
-        <Route path="/notesId" element={<Notes tsData = {tsData} patientId={patientId} />} />
+        <Route path="/notesId" element={<Notes />} />
       </Routes>
     </BrowserRouter>
   );
-}
 }
 
 export default App;
--- src/pages/Notes/Notes.js
+++ src/pages/Notes/Notes.js
@@ -1,67 +1,88 @@
 import "./Notes.scss";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Recorder from "../../components/Recorder/Recorder";
 
-function Notes({ tsData, patientId }) {
-  console.log(tsData)
-  console.log(patientId)
+function Notes() {
+  const [tsData, settsData] = useState("asd");
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080")
+      .then((res) => {
+        settsData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   {
     tsData
       .filter((data) => data.id == patientId)
       .map((data) => {
-        return (
-          <>
-            <div className="notes"> 
-              <div className="notes__header">
-                <img
-                  className="notes__logo"
-                  src={require("../../assets/Images/Logo.png")}
-                />
-                <button className="notes__record-button">
-                  Record new Patient Note
-                </button>
-              </div>
-              <div className="notes__container">
-                <div className="notes__patient">
-                  <div className="notes__patient-info">
-                    <div className="notes__patient-infotop">
-                      <div className="notes__patient-name">
-                        Patient Name: Lydia Shan 
+        if (tsData) {
+          return (
+            <>
+              <Recorder />
+              <div className="notes">
+                <div className="notes__header">
+                  <img
+                    className="notes__logo"
+                    src={require("../../assets/Images/Logo.png")}
+                  />
+                  <button className="notes__record-button">
+                    Record new Patient Note
+                  </button>
+                </div>
+                <div className="notes__container">
+                  <div className="notes__patient">
+                    <div className="notes__patient-info">
+                      <div className="notes__patient-infotop">
+                        <div className="notes__patient-name">
+                          Patient Name: Lydia Shan
+                        </div>
+                        <div className="notes__patient-date">Oct. 15, 2022</div>
                       </div>
-                      <div className="notes__patient-date">Oct. 15, 2022</div>
+                      <div className="notes__patient-infobottom">
+                        <div className="notes__patient-type">
+                          Type of Appointment: General Visit
+                        </div>
+                        <div className="notes__patient-purpose">
+                          Purpose of Visit: Abdominal Pain
+                        </div>
+                      </div>
                     </div>
-                    <div className="notes__patient-infobottom">
-                      <div className="notes__patient-type">
-                        Type of Appointment: General Visit
+                    <div className="notes__bottom">
+                      <div className="notes__patient-notes">
+                        <h2 className="notes__patient-heading">
+                          Doctor's Note
+                        </h2>
+                        <p>{tsData.data.text}</p>
                       </div>
-                      <div className="notes__patient-purpose">
-                        Purpose of Visit: Abdominal Pain
+                      <div className="notes__categories">
+                        <div className="notes__keywords">Keywords</div>
                       </div>
                     </div>
                   </div>
-                  <div className="notes__bottom">
-                    <div className="notes__patient-notes">
-                      <h2 className="notes__patient-heading">Doctor's Note</h2>
-                      <p>${data.text}</p>
-                    </div>
-                    <div className="notes__categories">
-                      <div className="notes__keywords">Keywords</div>
+                  <div className="notes__buttons">
+                    <button className="notes__buttons-delete">Delete</button>
+                    <div className="notes__buttons-container">
+                      <button className="notes__buttons-record">
+                        Record Again
+                      </button>
+                      <button className="notes__buttons-edit">Edit Note</button>
+                      <button className="notes__buttons-share">
+                        Share Note
+                      </button>
                     </div>
                   </div>
                 </div>
-                <div className="notes__buttons">
-                  <button className="notes__buttons-delete">Delete</button>
-                  <div className="notes__buttons-container">
-                    <button className="notes__buttons-record">
-                      Record Again
-                    </button>
-                    <button className="notes__buttons-edit">Edit Note</button>
-                    <button className="notes__buttons-share">Share Note</button>
-                  </div>
-                </div>
               </div>
-            </div>
-          </>
-        );
+            </>
+          );
+        }
       });
   }
 }
+
 export default Notes;
