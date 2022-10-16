6f3efbdf06f2b686b96ea356da3d20588cde60a5
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-10-16 00:41:18 -0400

    resolved merge conflicts in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+<<<<<<< HEAD
 
 import Summary from './pages/Summary/Summary';
 import Notes from './pages/Notes/Notes';
@@ -8,45 +9,22 @@ import Header from './components/Header/Header'
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+=======
+import Summary from "./pages/Summary/Summary";
+import Notes from "./pages/Notes/Notes";
+>>>>>>> 046fa30 (retrieve transcribed data from backend working)
 
 function App() {
-  const [tsData, settsData] = useState("");
-  const [patientId, setpatientId] = useState(null)
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080")
-      .then((res) => {
-        settsData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:8080")
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-  // console.log(tsData)
-  // console.log(patientId)
-if (patientId != null) {
 
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path="/" element={<Summary />} />
-        <Route path="/notesId" element={<Notes tsData = {tsData} patientId={patientId} />} />
+        <Route path="/notesId" element={<Notes />} />
       </Routes>
     </BrowserRouter>
   );
-}
 }
 
 export default App;
--- src/pages/Notes/Notes.js
+++ src/pages/Notes/Notes.js
@@ -1,5 +1,22 @@
 import "./Notes.scss";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Recorder from "../../components/Recorder/Recorder";
 
+function Notes() {
+  const [tsData, settsData] = useState("asd");
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080")
+      .then((res) => {
+        settsData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+<<<<<<< HEAD
 
 function Notes({ tsData, patientId }) {
     tsData
@@ -24,18 +41,57 @@ function Notes({ tsData, patientId }) {
                     <div className="notes__patient-infotop">
                       <div className="notes__patient-name">
                         Patient Name: Lydia Shan 
+=======
+  {
+    tsData
+      .filter((data) => data.id == patientId)
+      .map((data) => {
+        if (tsData) {
+          return (
+            <>
+              <Recorder />
+              <div className="notes">
+                <div className="notes__header">
+                  <img
+                    className="notes__logo"
+                    src={require("../../assets/Images/Logo.png")}
+                  />
+                  <button className="notes__record-button">
+                    Record new Patient Note
+                  </button>
+                </div>
+                <div className="notes__container">
+                  <div className="notes__patient">
+                    <div className="notes__patient-info">
+                      <div className="notes__patient-infotop">
+                        <div className="notes__patient-name">
+                          Patient Name: Lydia Shan
+                        </div>
+                        <div className="notes__patient-date">Oct. 15, 2022</div>
                       </div>
-                      <div className="notes__patient-date">Oct. 15, 2022</div>
+                      <div className="notes__patient-infobottom">
+                        <div className="notes__patient-type">
+                          Type of Appointment: General Visit
+                        </div>
+                        <div className="notes__patient-purpose">
+                          Purpose of Visit: Abdominal Pain
+                        </div>
+>>>>>>> 046fa30 (retrieve transcribed data from backend working)
+                      </div>
                     </div>
-                    <div className="notes__patient-infobottom">
-                      <div className="notes__patient-type">
-                        Type of Appointment: General Visit
+                    <div className="notes__bottom">
+                      <div className="notes__patient-notes">
+                        <h2 className="notes__patient-heading">
+                          Doctor's Note
+                        </h2>
+                        <p>{tsData.data.text}</p>
                       </div>
-                      <div className="notes__patient-purpose">
-                        Purpose of Visit: Abdominal Pain
+                      <div className="notes__categories">
+                        <div className="notes__keywords">Keywords</div>
                       </div>
                     </div>
                   </div>
+<<<<<<< HEAD
 
             <div className="notes__bottom">
               <div className="notes__patient-notes">
@@ -61,6 +117,27 @@ function Notes({ tsData, patientId }) {
     </>
   )});
 
+=======
+                  <div className="notes__buttons">
+                    <button className="notes__buttons-delete">Delete</button>
+                    <div className="notes__buttons-container">
+                      <button className="notes__buttons-record">
+                        Record Again
+                      </button>
+                      <button className="notes__buttons-edit">Edit Note</button>
+                      <button className="notes__buttons-share">
+                        Share Note
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        }
+      });
+  }
+>>>>>>> 046fa30 (retrieve transcribed data from backend working)
 }
 
 export default Notes;
