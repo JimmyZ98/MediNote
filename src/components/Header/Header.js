b5a6697d6fb2c53f978c940eb0dae1f77b061e13
Irene Xing <irenexing0220@gmail.com>
2022-10-16 05:16:27 -0400

    adding categories

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,32 @@
-import './Header.scss'
+import "./Header.scss";
+import UploadModal from "../UploadModal/UploadModal";
+import { useState } from "react";
 
-function Header (){
-return ( <div className="header">
-<img
-  className="header__logo"
-  src={require("../../assets/Images/Logo.png")}
-/>
-<button className="header__record-button">
-  Record New Note
-</button>
-</div>)}
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [upload, setUpload] = useState({});
 
-export default Header; 
+  const handleUpload = (event) => {
+    event.preventDefault();
+    setIsOpen(true);
+    setUpload(event);
+  };
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src={require("../../assets/Images/Logo.png")}
+      />
+      <button className="header__record-button" onClick={handleUpload}>
+        Record New Note
+      </button>
+      <UploadModal
+        openModal={isOpen}
+        closeModal={() => setIsOpen(false)}
+        upload={upload}
+      />
+    </div>
+  );
+}
+
+export default Header;
